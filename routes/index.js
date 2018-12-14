d5769321d2a035452ab43a03d4dfda558c4413d6
Xuân Tin <xuantinfx@gmail.com>
2018-12-15 01:01:56 +0700

    api broadcast tx

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var profileRouter = require('./profile')
+const transactionRouter = require('./transaction')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,5 +9,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.use('/profile', profileRouter);
+
+router.use('/transaction', transactionRouter)
 
 module.exports = router;
