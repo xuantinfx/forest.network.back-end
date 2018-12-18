3a2244314fe59473e952a5c08ed0caad32e06990
Vi Van <vivanpro1401@gmail.com>
2018-12-18 16:41:55 +0700

    fix tweets

--- controller/tweet/getTweets.js
+++ controller/tweet/getTweets.js
@@ -6,8 +6,10 @@ module.exports = (req, res, next) => {
     account.findOne({address: req.params.address},{tweets:1, name: 1, picture:1},
     (err, account)=> {
         if(account){
-            if(!account.tweets)
-                account.tweets = []
+            account.tweets = account.tweets||[]
+            account.name = account.name||''
+            account.picture = account.picture||''
+            
             responseData(res, account, 200, {})
         }
         else{
