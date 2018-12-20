9f4700b816b5e19a986c0e6cdb470dd6afa9a2b0
Dong-Nhan <ddnn97@hotmail.com>
2018-12-20 16:01:45 +0700

    sửa String -> Buffer của picture trong model

--- models/Account.js
+++ models/Account.js
@@ -10,7 +10,7 @@ let AccountSchema = new mongoose.Schema({
   bandwidthTime: { type: Number },
   name: { type: String },
   username: { type: String },
-  picture: { type: String },
+  picture: { type: Buffer },
   coverPhotoUrl: { type: String },
   bio: { type: String },
   location: { type: String },
