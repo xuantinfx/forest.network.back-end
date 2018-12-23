c828e41b3ca43d645e44af9a60eaadd17d18f784
Đào Xuân Tin <34159561+xuantinfx@users.noreply.github.com>
2018-12-23 14:50:33 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    connectionString: "mongodb+srv://forestnetwork1:Zxcasd123%40@cluster0-o0ots.mongodb.net/forestnetwork?retryWrites=true",
+    nodeHost: 'wss://komodo.forest.network:443',
+    syncBlock: true
+}
